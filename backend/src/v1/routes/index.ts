a6b1e5b10ac71f267730b0245cf371288e5e9e8f
Saqlain <riyanahmed1703@gmail.com>
2024-09-20 15:48:17 +0530

    fixing order of sending cookie

--- backend/src/v1/routes/index.ts
+++ backend/src/v1/routes/index.ts
@@ -19,10 +19,10 @@ app.use(cors({
 
 
 app.use(helmet())   
+app.use(express.json())
 app.use(cookieParser());
 
 //app.use('/api', limiter)
-app.use(express.json())
 app.get("/", (req, res)=>{
     res.send("Hello World")
 })
