b847967e3a6ec49819a2021e9ff9eaefdeca170b
Saqlain <riyanahmed1703@gmail.com>
2024-09-20 12:16:22 +0530

    added frontend urls

--- backend/src/v1/routes/index.ts
+++ backend/src/v1/routes/index.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin:["https://cartcraze.vercel.app", "http://localhost:5173"]
 }));
 //app.use('/api', limiter)
 app.use(express.json())
